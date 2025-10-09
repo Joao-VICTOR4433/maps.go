855e4b68a304379afc06199fb2b76d42536601ce
joão victor <joao.nogueira@dcx.ufpb.br>
2025-10-09 18:33:33 -0300

    Primeiro commit do projeto Go

---
+++ APLICA/aplica.go
@@ -0,0 +1,7 @@
+package APLICA
+
+import "fmt"
+
+func BemVindo() {
+    fmt.Println("Bem-vindo ao pacote APLICA!")
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+    "fmt"
+    "GOLANG/APLICA"
+)
+
+func main() {
+    fmt.Println("Chamando função do pacote APLICA:")
+    APLICA.BemVindo()
+}
